ea18ff43f1066637a3b13b95cc038e123a045e40
Abbas <accessemail111@gmail.com>
2022-08-09 18:44:20 +0430

    Add project files.

---
+++ CrudWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
