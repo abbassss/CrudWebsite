6e86c9f77bc85d699e2331103d20ec9ab3196f51
Abbas <accessemail111@gmail.com>
2022-08-09 21:09:11 +0430

    Some UI changes in Create form

--- CrudWebsite/Models/Transaction.cs
+++ CrudWebsite/Models/Transaction.cs
@@ -14,15 +14,19 @@ namespace CrudWebsite.Models
         public int Id { get; set; }
 
         [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Account Number")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
         public string BankName { get; set; }
 
         [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
         public string SWIFTCode { get; set; }
 
         public int Amount { get; set; }
